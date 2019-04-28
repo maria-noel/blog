79c28fb623325e25a01a2f38be39a5e57d54ecce
maria-noel <bacodesign@gmail.com>
2019-04-27 21:12:07 -0300

    Changes welcome screen title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Nerd Playground!
                 </div>
 
                 <div class="links">
