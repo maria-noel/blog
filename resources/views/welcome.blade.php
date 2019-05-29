effcf7b1a15ac9813f616061d67852f2ee988d11
maria-noel <bacodesign@gmail.com>
2019-05-29 12:19:19 -0300

    Tests rollback in fabfile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Nerd Playground!
+                    <small> Rollback funcionando </small>
                 </div>
 
                 <div class="links">
