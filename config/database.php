6298d787f24d45ae751b57d0c3f1af1a0ee20882
maria-noel <bacodesign@gmail.com>
2019-06-14 18:12:09 -0300

    Fix to dabase for codeship

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
         ],
         'mysql_tests' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE_TESTS', 'blog_tests'),
             'username' => env('MYSQL_USER', env('DB_USERNAME', 'forge')),
