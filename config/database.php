7a786ab82eaf41d1af5682ac4bc606e435eb4708
maria-noel <bacodesign@gmail.com>
2019-06-17 20:40:33 -0300

    Fixes codeship configuration number 10011

--- config/database.php
+++ config/database.php
@@ -56,7 +56,9 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE_TESTS', 'blog_tests'),
-            'username' => env('MYSQL_USERNAME', env('DB_USERNAME', 'forge')),
+            'username' => env('MYSQL_USER',env('DB_USERNAME', 'forge')),
+
+            #'username' => env('MYSQL_USERNAME', env('DB_USERNAME', 'forge')),
             'password' => env('MYSQL_PASSWORD', env('DB_PASSWORD', '')),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
