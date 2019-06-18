2a7d1853fa14895f436afeb1431b18b46ab6dcd8
maria-noel <bacodesign@gmail.com>
2019-06-17 22:07:47 -0300

    Testing codeship conf

--- config/database.php
+++ config/database.php
@@ -57,9 +57,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE_TESTS', 'blog_tests'),
             'username' => env('MYSQL_USER',env('DB_USERNAME', 'forge')),
-
-            #'username' => env('MYSQL_USERNAME', env('DB_USERNAME', 'forge')),
-            'password' => env('MYSQL_PASSWORD', env('DB_PASSWORD', '')),
+            'password' => env('MYSQL_PASSWORD',env('DB_PASSWORD', '')),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
